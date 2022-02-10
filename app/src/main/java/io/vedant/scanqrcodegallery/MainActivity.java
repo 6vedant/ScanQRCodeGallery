d6ad864c590382ba0dbf536674f6c0420340496b
vj <vedantj@nova.tech>
2022-02-10 13:34:29 +0530

    Added Gallery and Scan options

--- app/src/main/java/io/vedant/scanqrcodegallery/MainActivity.java
+++ app/src/main/java/io/vedant/scanqrcodegallery/MainActivity.java
@@ -1,5 +1,6 @@
 package io.vedant.scanqrcodegallery;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -18,6 +19,8 @@ import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.Reader;
 import com.google.zxing.Result;
 import com.google.zxing.common.HybridBinarizer;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -25,19 +28,47 @@ import java.io.InputStream;
 public class MainActivity extends AppCompatActivity {
     private static final int PICK_IMAGE = 333;
     private static final String TAG = "GALLERY";
-
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+            result -> {
+                if(result.getContents() == null) {
+                    Toast.makeText(MainActivity.this, "Cancelled", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.scan_qr).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // open camera to scan a qr/barcode
+
+                barcodeLauncher.launch(new ScanOptions());
+            }
+        });
 
         findViewById(R.id.select_image_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+
                 // select image
                 Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(intent, PICK_IMAGE);
+
+
+//                try {
+//
+//                    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+//                    intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+//                    startActivityForResult(intent, 0);
+//
+//                } catch (Exception e) {
+//                }
 
             }
         });
@@ -46,7 +77,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
 
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                Toast.makeText(getApplicationContext(), "Contents: "+contents, Toast.LENGTH_SHORT).show();
+            }
+            if(resultCode == RESULT_CANCELED){
+                //handle cancel
+            }
+        }
 
         if (requestCode == PICK_IMAGE && data != null) {
             // it is the success
